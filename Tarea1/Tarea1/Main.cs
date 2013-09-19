bd867a4dd797e97d3bd43bc826e49f0e733bc5a0
Ricardo Tapia <RickTC@localhost.localdomain>
2013-09-18 22:06:30 -0500

    main listo.

--- Tarea1/Tarea1/Main.cs
+++ Tarea1/Tarea1/Main.cs
@@ -6,7 +6,26 @@ namespace Tarea1
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Persona p= new Persona();
+			
+			Console.WriteLine ("Datos");
+			
+				Console.WriteLine("Ingrese su nombre");
+				p.nombre=Console.ReadLine();
+					
+					Console.WriteLine("Ingrese su edad");
+					p.edad=Console.ReadLine();
+						
+						Console.WriteLine("Ingrese su correo electronico");
+						p.correo=Console.ReadLine();
+			
+							Console.WriteLine("Sus datos son:");
+							Console.WriteLine("Nombre: "+ p.nombre);
+							Console.WriteLine("Edad: "+ p.edad);
+							Console.WriteLine("Correo: "+ p.correo);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
 		}
 	}
 }
